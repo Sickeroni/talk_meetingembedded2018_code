cc94e3ddc89e116edce96f699fc4c93cb2892164
Paul Targosz <Sickeroni@users.noreply.github.com>
2018-11-11 00:48:32 +0100

    kvasir added

---
+++ x86/SFR_wrapper.hpp
@@ -0,0 +1,31 @@
+#include <Register/Seam.hpp>
+// needed dependency for seamlib
+#include <Common/Tags.hpp>
+
+// stringfunctions to format output
+#include <bitset>
+#include <iomanip>
+#include <iostream>
+
+namespace Kvasir
+{
+    namespace Register
+    {
+        template <typename T>
+        struct ExecuteSeam<T, ::Kvasir::Tag::User> : RecordActions<T>
+        {
+            ExecuteSeam() {
+                for (auto iter : Kvasir::Register::actions_)
+                {
+                    std::cout << "mww 0x" << std::setfill('0') << std::hex << std::setw(8) << iter.address_;
+                    std::cout << " 0x" << std::setfill('0') << std::hex << std::setw(8) << iter.value_ << "\n";
+                }
+                Kvasir::Register::actions_.clear();
+            }
+        };
+    }
+}
+void seamoutput()
+{
+
+}
---
+++ x86/real_ledpanel.cpp
@@ -0,0 +1,36 @@
+// wrapper header for kvasir
+#include "SFR_wrapper.hpp"
+
+// kvasir header
+#include "Chip/MK64F12.hpp"
+
+extern "C" {
+
+
+int main(void); // needed here to be callable in main.c (otherwise name mangeling mismatch)
+}
+
+
+int main()
+{
+    //halt cpu
+    std::cout << "halt" << std::endl;
+
+    /* Port C Clock Gate Control: Clock enabled */
+    apply(write(Kvasir::SimScgc5::PortcValC::v1));
+    seamoutput();
+
+    /* PORTC16 is configured as GPIO*/
+    constexpr static auto pin_to_toggle = makePinLocation(Kvasir::Io::portC, Kvasir::Io::pin16);
+    apply(write(Kvasir::PortcPcr16::MuxValC::v001));
+
+    /* Port C16 to Output */
+    apply(makeOutput(pin_to_toggle));
+
+    while (1)
+    {
+        apply(write(Kvasir::PtcPtor::ptto,
+                    Kvasir::Register::value<(1<<16)>()));
+      //  GPIO_PortToggle(PIN_TO_TOGGLE_PORT, 1u << PIN_TO_TOGGLE_PIN);
+    }
+}
