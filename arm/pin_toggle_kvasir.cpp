ed38cb3529a5c582689eee227d2f9c7885ee3e4b
Paul Targosz <Sickeroni@users.noreply.github.com>
2018-11-10 22:11:50 +0100

    added kvasir benchmark

--- arm/pin_toggle_kvasir.cpp
+++ arm/pin_toggle_kvasir.cpp
@@ -12,8 +12,20 @@ int main(void); // needed here to be callable in main.c (otherwise name mangelin
 
 int main()
 {
+
+    /* Port C Clock Gate Control: Clock enabled */
+    apply(write(Kvasir::SimScgc5::PortcValC::v1));
+
+    /* PORTC16 is configured as GPIO*/
+    constexpr static auto pin_to_toggle = makePinLocation(Kvasir::Io::portC, Kvasir::Io::pin16);
+    apply(write(Kvasir::PortcPcr16::MuxValC::v001));
+    /* Port C16 to Output */
+    apply(makeOutput(pin_to_toggle));
+
     while (1)
     {
-
+        apply(write(Kvasir::PtcPtor::ptto,
+                    Kvasir::Register::value<(1<<16)>()));
+      //  GPIO_PortToggle(PIN_TO_TOGGLE_PORT, 1u << PIN_TO_TOGGLE_PIN);
     }
 }
