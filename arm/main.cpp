4afc6d55affbff291e69f1e99936b26ab5743378
Paul Targosz <Sickeroni@users.noreply.github.com>
2018-11-10 20:39:06 +0100

    added arm-project

---
+++ arm/main.cpp
@@ -0,0 +1,19 @@
+// kinetis SDK header
+#include <MK64F12.h>
+// kvasir header
+#include "Chip/MK64F12.hpp"
+
+extern "C" {
+
+
+int main(void); // needed here to be callable in main.c (otherwise name mangeling mismatch)
+}
+
+
+int main()
+{
+    while (1)
+    {
+
+    }
+}
